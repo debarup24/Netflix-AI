143067edd5ab7d61272ad1d4470387b73f925e59
Debarup <87480898+debarup24@users.noreply.github.com>
2024-11-27 11:37:58 +0530

    Gemini API setup

--- src/utils/Openai.js
+++ src/utils/Openai.js
@@ -1,8 +1,6 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import { OPENAI_KEY } from "./constant"
 
-
-// import { GoogleGenerativeAI } from "@google/generative-ai";
 const Openai = new GoogleGenerativeAI(OPENAI_KEY);
 
 export default Openai;
