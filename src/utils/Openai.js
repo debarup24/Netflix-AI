c69091d72327249b69f61a6c6cd6c8afe15146b0
Debarup <87480898+debarup24@users.noreply.github.com>
2025-02-09 21:46:56 +0530

    Update Openai.js

--- src/utils/Openai.js
+++ src/utils/Openai.js
@@ -2,6 +2,6 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 import { OPENAI_KEY } from "./constant"
 
 const Openai = new GoogleGenerativeAI(OPENAI_KEY);
-//AIzaSyA_5VldJjSBKRqv7DPKtsdcy5CR0J5DbiM 
+
 
 export default Openai;
